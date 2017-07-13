e281579316d43ea308cc3d91c8f9a25c944a027a
Ahmad Afandi <ahmad.afandi85@gmail.com>
2017-07-13 12:43:36 +0700

    menambahkan dokumentasi melalui apidoc (tahap awal)

--- application/controllers/api/Artikel.php
+++ application/controllers/api/Artikel.php
@@ -2,8 +2,58 @@
 
 class Artikel extends MY_Controller
 {
-  /** content selalu berbentuk array 2 dimensi
-  */
+  /**
+ * @api {get} /artikel/headline Mengambil artikel yang dijadikan sebagai headline
+ * @apiName GetHeadline
+ * @apiGroup Artikel
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *      "status": 1,
+ *      "content": [
+  *          {
+  *            "id": "99",
+  *          "gambar": "1472228892Raja Lombok 1902.jpg",
+  *          "isi": "<p style=\"text-align: justify;\" align=\"center\">Sejarah telah mencatat bahwa Pulau Lombok pernah menjadi wilayah kekuasaan Kerajaan Karang Asem Bali yang berkedudukan di Cakranegara dengan seorang raja bernama Anak Agung Gde Jelantik. Berakhirnya <strong>kekuasaan</strong>
+  *          "enabled": "1",
+  *          "tgl_upload": "2016-08-26 15:38:09",
+  *          "id_kategori": "999",
+  *          "id_user": "1",
+  *          "judul": "Sejarah Desa",
+  *          "headline": "1",
+  *          "gambar1": "1472229325490125_20121123041539.jpg",
+  *          "gambar2": "1472197089",
+  *          "gambar3": "1472197089",
+  *          "dokumen": "",
+  *          "link_dokumen": "",
+  *          "urut": "0",
+  *          "user": {
+  *            "id": "1",
+  *            "id_grup": "1",
+  *            "email": "admin@combine.or.id",
+  *            "last_login": "2017-05-01 13:04:01",
+  *            "active": "1",
+  *            "nama": "Administrator",
+  *            "company": "ADMIN",
+  *            "phone": "321",
+  *            "foto": "favicon.png"
+  *          }
+  *        }
+  *      ]
+  *      }
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
   public function headline_get(){
     $this->load->model('api/Artikel_model','artikel');
     $_data = $this->artikel->with('user')
@@ -51,7 +101,7 @@ class Artikel extends MY_Controller
     $page = $this->get('page');
     $limit = $this->get('limit');
     $page = empty($page) ? 1 : $page;
-    $limit = empty($limit) ? 1 : $limit;
+    $limit = empty($limit) ? 10 : $limit;
     $_data = $this->artikel->order_by('tgl_upload','desc')
                             ->paginate($limit,NULL,$page);
 
---
+++ doc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "API OpenSID",
+  "version": "0.1.0",
+  "description": "API untuk OpenSID",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-07-12T09:50:54.967Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
